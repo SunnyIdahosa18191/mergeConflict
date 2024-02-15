a217f27e22ff89ab7a68d4a5ad0da25a49636cfd
SunnyIdahosa18191 <SunnyIdahosa@protonmail.com>
2024-02-15 13:52:47 +0200

    add new helper functions

---
+++ Narcissistic Number/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Narcissistic_Number
+{
+    public class Kata
+    {
+        public static bool Narcissistic(int value)
+        {
+            int result = 0;
+            int additionalValue = value;
+
+            while (additionalValue > 0)
+            {
+                result += (int)Math.Pow(additionalValue % 10, value.ToString().Length);
+                additionalValue /= 10;
+            }
+            return result == value;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata kata = new Kata();
+            Console.WriteLine(Kata.Narcissistic(153));
+        }
+    }
+}
